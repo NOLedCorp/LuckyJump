f66823001666f23c64622050c3ded2f48f17a9b2
Иван Номоконов <171035@edu.fa.ru>
2018-12-07 18:14:09 +0300

    прыжок + границы обруча теперь отталкивают шар

--- src/app/game-field/game-field.component.ts
+++ src/app/game-field/game-field.component.ts
@@ -13,12 +13,14 @@ export class GameFieldComponent implements OnInit {
   ringRight:number = -300;
   ringSmallRight:number = -275;
   ctrl:any = this;
+  scored:boolean = false;
   constructor() { }
   ngOnInit() {
     var c = this;
     this.timerRing = setInterval(function(this){
       c.ringSmallRight+=1;
       if(c.ringSmallRight>950){
+        c.scored = false;
         c.ringSmallRight=-250;
       }
     }, 10)
@@ -26,7 +28,6 @@ export class GameFieldComponent implements OnInit {
 
   fall(ball:HTMLDivElement, ctrl:any){
     var start = Number(ball.style.top.replace('px',""));
-    console.log(ball.style.top);
     this.timerFall = setInterval(function() {
       var timePassed = Math.abs(Number(ball.style.top.replace('px',"")) - start);
 
@@ -35,16 +36,23 @@ export class GameFieldComponent implements OnInit {
         clearInterval(this.timerFall); 
         return;
       }
-      console.log(start);
-      console.log(ball.style.top);
+      ball.style.top = Number(ball.style.top.replace('px',""))+0.5+(timePassed==0? start / 100: timePassed / 100)+ 'px';
+      let t = Number(ball.style.top.replace('px',""));
+      let t1 = Number(ball.style.right.replace('px',""))
+      if (!ctrl.scored && t>230 && t<270 && ctrl.ringSmallRight>605 && ctrl.ringSmallRight<710  ) {
+        if(ctrl.ringSmallRight<615 || ctrl.ringSmallRight>700){
+          ctrl.jump(ball, ctrl);
+        }
+        else{
+          console.log(ctrl.ringSmallRight);
+          ctrl.scored=true;
+          ctrl.score++;
+        }
 
-      console.log(timePassed);
-
-      ball.style.top = Number(ball.style.top.replace('px',""))+1+(timePassed==0? start / 100: timePassed / 100)+ 'px';
-      let t = Number(ball.style.top.replace('px',""))
-      if (t>230 && t<270) {
-        ctrl.score++;
+        
       }
+      
+
 
     }, 5);
     
@@ -63,19 +71,18 @@ export class GameFieldComponent implements OnInit {
       
       if (Number(ball.style.top.replace('px',""))<=10) {
         clearInterval(ctrl.timerJump);
-
+        ctrl.fall(ball, ctrl);
          // конец через 2 секунды
         return;
       }
-
-      if (Math.abs(Number(ball.style.top.replace('px',""))-start)>120) {
+      if (timePassed>80) {
         clearInterval(ctrl.timerJump);
         ctrl.fall(ball, ctrl);
         return;
       }
 
       // рисует состояние анимации, соответствующее времени timePassed
-      ball.style.top = Number(ball.style.top.replace('px',""))- (timePassed>150?1:2) + 'px';
+      ball.style.top = Number(ball.style.top.replace('px',""))- (2-(timePassed==0? start / 50: timePassed / 50)) + 'px';
       
 
     }, 5);
