ceeaa171d31849f0948d466932fc71ae599f90e7
Иван Номоконов <171035@edu.fa.ru>
2018-12-09 01:08:39 +0300

    Сделал вращение

--- src/app/game-field/game-field.component.ts
+++ src/app/game-field/game-field.component.ts
@@ -10,6 +10,8 @@ export class GameFieldComponent implements OnInit {
   timerFall:any;
   timerJump:any;
   timerRing:any;
+  timerRotate:any;
+  curRotate:number=0;
   scrWidth:number=800;
   scrHeight:number=500;
   ringWidth:number = 100;
@@ -20,6 +22,7 @@ export class GameFieldComponent implements OnInit {
   ringSmallRight:number = -275;
   ctrl:any = this;
   scored:boolean = false;
+  rotating:boolean=false;
   falled:boolean = false;
   constructor() { }
   @ViewChild('ball')
@@ -31,6 +34,7 @@ export class GameFieldComponent implements OnInit {
   ngOnInit() {
 
     var c = this;
+    
     this.timerRing = setInterval(function(this){
       c.ringSmallRight+=1;
       if(c.ringSmallRight>c.scrWidth+c.ringWidth){
@@ -40,10 +44,10 @@ export class GameFieldComponent implements OnInit {
     }, 10);
     this.scrWidth=window.innerWidth;
     this.scrHeight=window.innerHeight;
-    console.log(this.scrHeight/2-10);
     this.m.nativeElement.style.top=this.scrHeight/2-15 + 'px';
     this.ringsTop = this.scrHeight/2-10;
     this.ballRight = this.scrWidth * 0.6;
+    // this.ringSmallRight= this.ballRight+6;
   }
 
   fall(ball:any, ctrl:any){
@@ -52,27 +56,41 @@ export class GameFieldComponent implements OnInit {
       var timePassed = Math.abs(Number(ball.style.top.replace('px',"")) - start);
 
       
-      if (Number(ball.style.top.replace('px',""))>=ctrl.scrHeight-30) {
+      if (Number(ball.style.top.replace('px',""))>=ctrl.scrHeight-35) {
         if(!ctrl.falled){
           ctrl.jump(ball, ctrl, (Number(ball.style.top.replace('px',"")) - start)/4);
         }
+        
         ctrl.score=0;
         ctrl.falled=true;
-        clearInterval(this.timerFall); 
+        clearInterval(ctrl.timerFall); 
         return;
       }
       ball.style.top = Number(ball.style.top.replace('px',""))+1+Math.pow(timePassed/100,1.4)+ 'px';
       let t = Number(ball.style.top.replace('px',""));
       let t1 = Number(ball.style.right.replace('px',""))
-      if (!ctrl.scored && t>ctrl.scrHeight/2-10 && t>ctrl.scrHeight/2 && ctrl.ringSmallRight>ctrl.ballRight+5-ctrl.ringWidth && ctrl.ringSmallRight<ctrl.ballRight+10  ) {
-        if(ctrl.ringSmallRight<ctrl.scrWidth+15-ctrl.ballRight || ctrl.ringSmallRight>ctrl.ballRight){
+      if (t>ctrl.scrHeight/2-30 && t<ctrl.scrHeight/2+10 && ctrl.ringSmallRight>ctrl.ballRight-ctrl.ringWidth+17 && ctrl.ringSmallRight<ctrl.ballRight+19) {
+        if(ctrl.ringSmallRight<ctrl.ballRight-ctrl.ringWidth+26 || ctrl.ringSmallRight>ctrl.ballRight+10){
+          
           ctrl.jump(ball, ctrl, (Number(ball.style.top.replace('px',"")) - start)/4);
         }
+        
         else{
-          ctrl.scored=true;
-          ctrl.score++;
+          if(!ctrl.scored ){
+            ctrl.scored=true;
+            ctrl.score++;
+          }
+          
         }
 
+        
+      }
+      if( !ctrl.rotating && t>ctrl.scrHeight/2-30 && t<ctrl.scrHeight/2+10 && (!ctrl.scored && ctrl.ringSmallRight>ctrl.ballRight-ctrl.ringWidth-1 && ctrl.ringSmallRight<ctrl.ballRight-ctrl.ringWidth+18 || ctrl.ringSmallRight<ctrl.ballRight+10 && ctrl.ringSmallRight>ctrl.ballRight+5)){
+        console.log(ctrl.ringSmallRight);
+        console.log(ctrl.ballRight-ctrl.ringWidth);
+        ctrl.ringSmallRight-= 4;
+        ctrl.rotating = true;
+        ctrl.rotate(ball,ctrl);
         
       }
       
@@ -80,9 +98,77 @@ export class GameFieldComponent implements OnInit {
 
     }, 5);
     
+    
   }
+  rotate(ball:HTMLDivElement, ctrl:any){
+  ctrl.curRotate = ctrl.curRotate%360;
+  console.log(ctrl.curRotate);
+  var start = ctrl.curRotate;
+  var timePassed=0;
+  clearInterval(this.timerRotate);
+  
+    this.timerRotate = setInterval(function() {
+     
+      
+      if(timePassed>500){
+        ctrl.rotating = false;
+        clearInterval(ctrl.timerRotate);
+        return;
+      }
+      ctrl.curRotate+=5;
+      console.log(ctrl.curRotate);
+      timePassed=ctrl.curRotate-start;
+      console.log(false);
+      ball.style.transform="rotate("+ctrl.curRotate+"deg)";
 
 
+    }, 5);
+    
+    
+  }
+  // rotate(ball:HTMLDivElement, ctrl:any){
+  // var start = ball.style.transform;
+  // console.log(start);
+    // this.timerFall = setInterval(function() {
+    //   var timePassed = Math.abs(Number(ball.style.top.replace('px',"")) - start);
+
+      
+    //   if (Number(ball.style.top.replace('px',""))>=ctrl.scrHeight-35) {
+    //     if(!ctrl.falled){
+    //       ctrl.jump(ball, ctrl, (Number(ball.style.top.replace('px',"")) - start)/4);
+    //     }
+    //     ctrl.score=0;
+    //     ctrl.falled=true;
+    //     clearInterval(ctrl.timerFall); 
+    //     return;
+    //   }
+    //   ball.style.top = Number(ball.style.top.replace('px',""))+1+Math.pow(timePassed/100,1.4)+ 'px';
+    //   let t = Number(ball.style.top.replace('px',""));
+    //   let t1 = Number(ball.style.right.replace('px',""))
+    //   if (t>ctrl.scrHeight/2-30 && t<ctrl.scrHeight/2+10 && ctrl.ringSmallRight>ctrl.ballRight-ctrl.ringWidth+17 && ctrl.ringSmallRight<ctrl.ballRight+19) {
+    //     if(ctrl.ringSmallRight<ctrl.ballRight-ctrl.ringWidth+26 || ctrl.ringSmallRight>ctrl.ballRight+10){
+    //       console.log(true);
+    //       ctrl.jump(ball, ctrl, (Number(ball.style.top.replace('px',"")) - start)/4);
+    //     }
+    //     else{
+    //       if(!ctrl.scored ){
+    //         ctrl.scored=true;
+    //         ctrl.score++;
+    //       }
+          
+    //     }
+
+        
+    //   }
+      
+
+
+    // }, 5);
+    
+    
+  //}
+
+  
   jump(ball:any, ctrl:any, pos:number = 100){
     clearInterval(this.timerFall);
     clearInterval(this.timerJump);
@@ -108,7 +194,15 @@ export class GameFieldComponent implements OnInit {
         ctrl.fall(ball, ctrl);
         return;
       }
-
+      let t = Number(ball.style.top.replace('px',""));
+      if( !ctrl.rotating && t>ctrl.scrHeight/2-30 && t<ctrl.scrHeight/2+10 && (!ctrl.scored && ctrl.ringSmallRight>ctrl.ballRight-ctrl.ringWidth-1 && ctrl.ringSmallRight<ctrl.ballRight-ctrl.ringWidth+18 || ctrl.ringSmallRight<ctrl.ballRight+10 && ctrl.ringSmallRight>ctrl.ballRight+5)){
+        console.log(ctrl.ringSmallRight);
+        console.log(ctrl.ballRight-ctrl.ringWidth);
+        ctrl.ringSmallRight-= 4;
+        ctrl.rotating = true;
+        ctrl.rotate(ball,ctrl);
+        
+      }
       // рисует состояние анимации, соответствующее времени timePassed
       ball.style.top = Number(ball.style.top.replace('px',""))- (3-Math.pow(timePassed/100,2)) + 'px';
    
